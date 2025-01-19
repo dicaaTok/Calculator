7301506b231a52e623e8a4c74fecffabf4a72ef5
dicaaTok <dicaaGeek@gmail.com>
2025-01-19 21:03:29 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args ) {
+
+
+    Scanner sc = new Scanner(System.in);
+        System.out.printf("ведите число");
+        float a = sc.nextFloat();
+        System.out.printf("ведите опирацию (+,-,*,/)");
+        char operation = sc.next().charAt(0);
+        System.out.printf("ведите второе число");
+        float b = sc.nextFloat();
+
+        if (operation == '+') {
+            float result = a + b;
+            System.out.printf(a + "+" + b + "=" + result);
+        } else if (operation == '-') {
+            float result = a - b;
+            System.out.printf(a + "-" + b + "=" + result);
+        } else if (operation == '*') {
+            float result = a * b;
+            System.out.printf(a + "*" + b + "=" + result);
+        } else if (operation == '/') {
+            float result = a / b;
+            System.out.printf(a + "/" + b + "=" + result);
+        } else {
+            System.out.printf("калкулятор не поерживет такую функцию");
+        }
+
+        }
+
+    }
